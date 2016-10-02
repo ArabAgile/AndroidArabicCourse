12a3ffdf9f00c19bfe62fba9be9041cffa618978
Trainer <trainer@ammars-mbp.domain.com>
2016-10-02 16:34:46 +0400

    Enable back for details activity

--- app/src/main/java/com/arabagile/diwan/DetailsActivity.java
+++ app/src/main/java/com/arabagile/diwan/DetailsActivity.java
@@ -2,6 +2,7 @@ package com.arabagile.diwan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by trainer on 10/1/16.
@@ -13,5 +14,18 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // I do not want this...
+                // Home as up button is to navigate to Home-Activity not previous acitivity
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
